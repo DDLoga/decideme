f52ab6dfd9693b3650800ebcf7a93d2d64f49066
DDLoga <ds3782@protonmail.com>
2024-08-02 17:09:43 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})({
+  // Add any other configuration options here
+});
 
 export default nextConfig;
